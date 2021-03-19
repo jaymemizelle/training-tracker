213f42c72ebdff1cdcce90573c44d04cfd0f7b9c
Pro-Code-art <jayme.coder@gmail.com>
2021-03-18 18:31:34 -0700

    all heroku issues solved

--- seeders/seed.js
+++ seeders/seed.js
@@ -2,7 +2,7 @@ let mongoose = require("mongoose");
 let db = require("../models");
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  process.env.MONGODB_URI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
